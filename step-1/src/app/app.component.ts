e74e2d354c6e7e1c441a4bb3ddebe6aae6bde445
Anton Moiseev <a@antonmoiseev.com>
2017-03-04 15:36:03 +0400

    Add toolbar instructions into custom angular material step

--- step-1/src/app/app.component.ts
+++ step-1/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { ProductService } from './shared/services';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'ngs-app',
-  styleUrls: [ './app.component.css' ],
-  templateUrl: './app.component.html'
+  styleUrls: [ './app.component.scss' ],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(productService: ProductService) {
-    productService.getAll().subscribe(products => console.log(products));
+
+  constructor(
+      private domSanitizer: DomSanitizer,
+      private iconRegistry: MdIconRegistry) {
+    this.registerIcons(new Map<string, string>([
+      [ 'logo', 'assets/ngshop-logo.svg' ]
+    ]));
+  }
+
+  private registerIcons(icons: Map<string, string>) {
+    icons.forEach((url, id) => {
+      const safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+      this.iconRegistry.addSvgIconInNamespace('ngs', id, safeUrl);
+    });
   }
 }
