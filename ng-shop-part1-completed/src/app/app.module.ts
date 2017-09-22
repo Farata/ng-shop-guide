bc918bca602464875312f16acdd65b56e778166d
Yakov Fain <yakovfain@gmail.com>
2017-09-22 11:54:42 -0400

    added comments on breaking changes in beta.11

--- ng-shop-part1-completed/src/app/app.module.ts
+++ ng-shop-part1-completed/src/app/app.module.ts
@@ -8,6 +8,18 @@ import {HttpClientModule} from "@angular/common/http";
 import { HomeComponent, ProductTileComponent  } from './home';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
+/*
+   Starting from Angular material 2.0.0-beta.11 the Md prefixes are deprecated. For example,
+   instead of using MdToolbarModule, you should be using MatTabModule.
+
+   Also, you should not be importing all modules from '@angular/material', but import each module from
+   an individual material module. For example, instead of this:
+
+   import {MdToolbarModule} from '@angular/material';
+
+   import {MatToolbarModule} from '@angular/material/toolbar';
+ */
+ 
 @NgModule({
   declarations: [
     AppComponent,
